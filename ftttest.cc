b12d3e5fe002ba952c97db3cc4c3e03bdc38b2f7
tricritical <lixinran2011@gmail.com>
2013-11-28 10:48:37 +0800

    some trying to read root file

--- ftttest.cc
+++ ftttest.cc
@@ -7,11 +7,17 @@ ClassImp(solve_data);
 ClassImp(input_sample);
 int main(){
 	TFile *f1=new TFile("data/3243.root");
-	TTree *t1=(TTree*)f1->Get("t1");
-	solve_data thedata;
-	t1->SetBranchAddress("data",&thedata);
-	t1->GetEntry(0);
-	cout<<thedata.data[0][1];
+	TTree *ptree=(TTree*)f1->Get("t1");
+	solve_data *thedata;
+	cout<<&thedata<<endl;
+	getchar();
+	ptree->SetBranchAddress("data",&thedata);
+	cout<<&thedata<<endl;
+	getchar();
+	ptree->GetEntry(1);
+	cout<<&thedata<<endl;
+	getchar();
+	cout<<thedata->inet<<"  "<<thedata->params.t_a<<"  "<<thedata->data[2][3];
 	
 //data
 //	t1->Show(0);
