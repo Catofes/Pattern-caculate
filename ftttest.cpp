f3536da8ac7b741164520e2191bfbd7ca76b733e
Li Xinran <lixinran2011@gmail.com>
2014-04-02 02:33:40 +0800

    make the plot better

--- ftttest.cpp
+++ ftttest.cpp
@@ -9,7 +9,6 @@
 #include"TColor.h"
 #include"TStyle.h"
 #include"TH2D.h"
-
 using namespace std;
 using namespace Array;
 using namespace fftwpp;
@@ -17,8 +16,25 @@ using namespace fftwpp;
 ClassImp(solve_data);
 ClassImp(input_sample);
 void draw(Double_t *data,int imap, Int_t x=20, Int_t y=20){
+	static Int_t colors[256];
+	static Bool_t initialized = kFALSE;
+	Double_t Red[3]  ={1.00,0.50,0.00};
+	Double_t Green[3]={1.00,0.50,0.00};
+	Double_t Blue[3] ={1.00,0.50,0.00};
+	Double_t Length[3]={0.00,0.50,1.00};
+	if(!initialized){
+		Int_t FI = TColor::CreateGradientColorTable(3, Length, Red, Green, Blue, 256);
+		for(int i=0;i<256;i++)colors[i]=FI+i;
+		initialized = kTRUE;
+	}
+	gStyle->SetNumberContours(256);
+	gStyle->SetPalette(256,colors);
 	TCanvas *c1=new TCanvas("c1","",400,400);
+//	gStyle->SetC
 	TH2D *h=new TH2D("h","",x,0,x,y,0,y);
+	h->SetStats(kFALSE);
+	h->SetMaximum(101);
+	h->SetMinimum(0);
 	for(Int_t i=0;i<x*y;i++){
 		for(Int_t jhight=0;jhight<data[i]*100;jhight++){
 			h->Fill(i%x+0.5,i/y+0.5);
@@ -31,7 +47,12 @@ void draw(Double_t *data,int imap, Int_t x=20, Int_t y=20){
 
 void drawraw(Double_t *data,int imap, Int_t x=20, Int_t y=20){
 	TCanvas *c1=new TCanvas("c1","",400,400);
+	gStyle->SetPalette(53,0);
+	gStyle->SetNumberContours(256);
 	TH2D *h=new TH2D("h","",x,0,x,y,0,y);
+	h->SetStats(kFALSE);
+	h->SetMaximum(101);
+	h->SetMinimum(0);
 	for(Int_t i=0;i<x*y;i++){
 		for(Int_t jhight=0;jhight<data[i]*100;jhight++){
 			h->Fill(i%x+0.5,i/y+0.5);
@@ -43,14 +64,19 @@ void drawraw(Double_t *data,int imap, Int_t x=20, Int_t y=20){
 }
 
 int ftttest(int imap) {
+cout<<"0.1"<<endl;
 	TString rootname=Form("%s%d%s","data/",imap,".root");
 	TFile *f1=new TFile(rootname);
 	TTree *tree=(TTree*)f1->Get("tree");
+cout<<"0.2"<<endl;
 	solve_data *thedata=0;
+cout<<"0.3"<<endl;
 	tree->SetBranchAddress("thedata",&thedata);
+cout<<"0.4"<<endl;
 	tree->GetEntry(0);
+cout<<"0.5"<<endl;
 	tree->Show();
-
+cout<<"1"<<endl;
 	fftw::maxthreads=get_max_threads();
 	unsigned int nx=20, ny=20;
 	size_t align=sizeof(Complex);
@@ -59,7 +85,7 @@ int ftttest(int imap) {
 
 	fft2d Forward2(-1,f);
 	fft2d Backward2(1,f);
-
+cout<<"2"<<endl;
 	for(unsigned int i=0; i < nx; i++) 
 	  for(unsigned int j=0; j < ny; j++) 
 		f(i,j)=thedata->data[0][20*i+j];
@@ -77,7 +103,7 @@ int ftttest(int imap) {
 
 	draw(finish,imap);
 	drawraw(thedata->data[0],imap);
-
+return 0;
 }
 
 int main(int argc, char **argv) {
