d83d3995ec298ce956ad249edc17d68ac52f5161
Catofes <trangent.tk@gmail.com>
2014-04-01 16:09:39 +0800

    change ftttest.cpp to draw fft picture and raw picture

--- ftttest.cpp
+++ ftttest.cpp
@@ -8,7 +8,7 @@
 #include"TGraph2D.h"
 #include"TColor.h"
 #include"TStyle.h"
-
+#include"TH2D.h"
 
 using namespace std;
 using namespace Array;
@@ -16,6 +16,32 @@ using namespace fftwpp;
 
 ClassImp(solve_data);
 ClassImp(input_sample);
+void draw(Double_t *data,int imap, Int_t x=20, Int_t y=20){
+	TCanvas *c1=new TCanvas("c1","",400,400);
+	TH2D *h=new TH2D("h","",x,0,x,y,0,y);
+	for(Int_t i=0;i<x*y;i++){
+		for(Int_t jhight=0;jhight<data[i]*100;jhight++){
+			h->Fill(i%x+0.5,i/y+0.5);
+		}
+	}
+	h->Draw("COLZ");
+	TString name=Form("%s%d%s","data/",imap,".fft.bmp");
+	c1->Print(name);
+}
+
+void drawraw(Double_t *data,int imap, Int_t x=20, Int_t y=20){
+	TCanvas *c1=new TCanvas("c1","",400,400);
+	TH2D *h=new TH2D("h","",x,0,x,y,0,y);
+	for(Int_t i=0;i<x*y;i++){
+		for(Int_t jhight=0;jhight<data[i]*100;jhight++){
+			h->Fill(i%x+0.5,i/y+0.5);
+		}
+	}   
+	h->Draw("COLZ");
+	TString name=Form("%s%d%s","data/",imap,".bmp");
+	c1->Print(name);
+}
+
 int ftttest(int imap) {
 	TString rootname=Form("%s%d%s","data/",imap,".root");
 	TFile *f1=new TFile(rootname);
@@ -39,40 +65,28 @@ int ftttest(int imap) {
 		f(i,j)=thedata->data[0][20*i+j];
 
 	Forward2.fft(f);
-    
+	cout<<f<<endl;
 
-	TCanvas *c1=new TCanvas("c1","",300,300);
-    gStyle->SetPalette(53,0);
-	gStyle->SetNumberContours(256);
+	double * finish=new double[400];
+	for(unsigned int i=0; i < nx; i++) 
+	  for(unsigned int j=0; j < ny; j++) 
+		finish[20*i+j]=sqrt(f(i,j).real()*f(i,j).real()+f(i,j).imag()*f(i,j).imag());
 
-	Double_t *a=new Double_t[400];
-	Double_t *x=new Double_t[400];
-	Double_t *y=new Double_t[400];
-	for(int iy=0;iy<20;iy++){
-		for(int jx=0;jx<20;jx++){
-			a[iy*20+jx]=f(jx,iy).real();
-			if((iy+jx)%2==0) a[iy*20+jx]=1;
-			else a[iy*20+jx]=0;
-			x[iy*20+jx]=jx+0.5;
-			y[iy*20+jx]=iy+0.5;
-		}
-	}   
-	TString name=Form("%s%d%s","data/",imap,"ftt.bmp");
+	for(int i=0;i<400;i++)
+	  cout<<finish[i]<<"	";
 
-	TGraph2D *graphA=new TGraph2D(400,x,y,a);
-	graphA->SetName("A");
-	graphA->SetNpx(20);
-	graphA->SetNpy(20);
-	graphA->SetMaximum(1.0001);
-	graphA->SetMinimum(-0.0001);
-	graphA->SetTitle(name);
-	graphA->Draw("COLZ");
-	c1->Update();
-    c1->Print(name);
+	draw(finish,imap);
+	drawraw(thedata->data[0],imap);
 
 }
 
-int main() {
-	ftttest(3243);
+int main(int argc, char **argv) {
+	int start=atoi(argv[1]);
+	int theend=atoi(argv[2]);
+	for(int i=start;i<theend;i++)
+	{   
+		cout<<i<<endl;
+		ftttest(i);
+	}  
 	return 0;
 }
