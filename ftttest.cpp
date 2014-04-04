3562bd5a9d5dae6a1d23e5e6018b061375f70060
Li Xinran <lixinran2011@gmail.com>
2014-04-05 07:32:07 +0800

    finished

--- ftttest.cpp
+++ ftttest.cpp
@@ -16,11 +16,9 @@ using namespace fftwpp;
 
 ClassImp(solve_data);
 ClassImp(input_sample);
-void draw(Double_t *data,Double_t *datain,int imap, Int_t x=20, Int_t y=20){
-	TCanvas *c1=new TCanvas("c1","",800,400);
+void draw(Double_t *data,Double_t *datain,TString name,TCanvas *c1,TH2D *h,TH2D *hin,Int_t x=20, Int_t y=20){
 	c1->Divide(2,1);
 	c1->cd(1);
-	TH2D *h=new TH2D("h","",x,0,x,y,0,y);
 	h->SetStats(kFALSE);
 	h->SetMinimum(-0.0001);
 	for(Int_t i=0;i<x*y;i++){
@@ -30,7 +28,6 @@ void draw(Double_t *data,Double_t *datain,int imap, Int_t x=20, Int_t y=20){
 	}
 	h->Draw("COLZ");
 	c1->cd(2);
-	TH2D *hin=new TH2D("hin","",x,0,x,y,0,y);
 	hin->SetStats(kFALSE);
 	hin->SetMinimum(-0.0001);
 	for(Int_t i=0;i<x*y;i++){
@@ -39,48 +36,51 @@ void draw(Double_t *data,Double_t *datain,int imap, Int_t x=20, Int_t y=20){
 		}
 	}
 	hin->Draw("COLZ");
-	
-	TString name=Form("%s%d%s","data/",imap,".fft.bmp");
+
 	c1->Print(name);
 }
 
 
-int ftttest(int size=20,int imap=0) {
+int ftttest(int size,int imap,TCanvas *c1,TH2D *h1,TH2D *h2) {
 	TString rootname=Form("%s%d%s","data/",imap,".root");
 	TFile *f1=new TFile(rootname);
 	TTree *tree=(TTree*)f1->Get("tree");
 	solve_data *thedata=0;
 	tree->SetBranchAddress("thedata",&thedata);
-	tree->GetEntry(0);
-	tree->Show();
 
-//	fftw::maxthreads=get_max_threads();
-	unsigned int nx=size,ny=size;//nx=20, ny=20;
-	size_t align=sizeof(Complex);
+	int nn=tree->GetEntries();
+	for ( int ni=0;ni<nn;ni++){
+		tree->GetEntry(ni);
+		tree->Show();
 
-	array2<Complex> f(nx,ny,align);
+		//	fftw::maxthreads=get_max_threads();
+		unsigned int nx=size,ny=size;//nx=20, ny=20;
+		size_t align=sizeof(Complex);
 
-	fft2d Forward2(-1,f);
+		array2<Complex> f(nx,ny,align);
 
-	for(unsigned int i=0; i < nx; i++) 
-	  for(unsigned int j=0; j < ny; j++) 
-		f(i,j)=thedata->data[0][2*(nx/2*(i/2)+j/2)];
-	Forward2.Shift(f,nx,ny,1,0);
-	Forward2.fft(f);
+		fft2d Forward2(-1,f);
 
-	double * finish=new double[nx*ny];
-	for(unsigned int i=0; i < nx; i++) 
-	  for(unsigned int j=0; j < ny; j++) 
-		finish[nx*i+j]=sqrt(f(i,j).real()*f(i,j).real()+f(i,j).imag()*f(i,j).imag());
+		for(unsigned int i=0; i < nx; i++) 
+		  for(unsigned int j=0; j < ny; j++) 
+			f(i,j)=thedata->data[0][2*(nx/2*(i/2)+j/2)];
+		Forward2.Shift(f,nx,ny,1,0);
+		Forward2.fft(f);
 
-	double * old=new double[nx*ny];
-	for(unsigned int i=0; i < nx; i++) 
-	  for(unsigned int j=0; j < ny; j++) 
-		old[nx*i+j]=thedata->data[0][2*(nx/2*(i/2)+j/2)];
-	
+		double  finish[nx*ny];
+		for(unsigned int i=0; i < nx; i++) 
+		  for(unsigned int j=0; j < ny; j++) 
+			finish[nx*i+j]=sqrt(f(i,j).real()*f(i,j).real()+f(i,j).imag()*f(i,j).imag());
 
-	draw(finish,old,imap,nx,ny);
+		double  old[nx*ny];
+		for(unsigned int i=0; i < nx; i++) 
+		  for(unsigned int j=0; j < ny; j++) 
+			old[nx*i+j]=thedata->data[0][2*(nx/2*(i/2)+j/2)];
 
+		TString name=Form("%s%d%s%d%s","data/",imap,"/",ni,".bmp");
+		c1->Clear();
+		draw(finish,old,name,c1,h1,h2,nx,ny);
+	}
 }
 
 int main(int argc, char **argv) {	
@@ -89,10 +89,15 @@ int main(int argc, char **argv) {
 	gStyle->SetPalette(53,0);
 	gStyle->SetNumberContours(256);
 
+	TCanvas *c1=new TCanvas("c1","",800,400);
+	TH2D *h1=new TH2D("h1","",40,0,40,40,0,40);
+	TH2D *h2=new TH2D("h2","",40,0,40,40,0,40);
+	h2->SetMaximum(100);
+	h2->SetMinimum(0);
 	for(int i=start;i<theend;i++)
 	{   
 		cout<<i<<endl;
-		ftttest(40,i);
+		ftttest(40,i,c1,h1,h2);
 	}  
 	return 0;
 }
